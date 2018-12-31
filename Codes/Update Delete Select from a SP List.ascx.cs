8c325988e9d7c854ae0db1c16258d30d3ac47c55
olgaczaim <46277089+olgaczaim@users.noreply.github.com>
2018-12-31 14:09:42 +0300

    Create Update Delete Select from a SP List.ascx.cs

---
+++ Codes/Update Delete Select from a SP List.ascx.cs
@@ -0,0 +1,71 @@
+
+//Select from a list
+SPSite thissite = SPContext.Current.Site;
+SPWeb thisweb = SPContext.Current.Web;
+//SPSecurity.RunWithElevatedPrivileges(delegate()
+//{
+using (SPSite currentSite = new SPSite(thissite.ID))
+{
+    using (SPWeb currentWeb = currentSite.OpenWeb(thisweb.ID))
+    {
+
+        SPList list = currentWeb.Lists["ListName"];
+
+        IEnumerable<SPListItem> items = (from SPListItem a in list.Items
+                                        where a["Title"].Equals("1")
+                                        orderby a["Title"] descending
+                                        select a);
+
+
+        foreach (SPListItem item in items)
+        {
+            string title = item["Title"].ToString();
+        }
+
+    }
+}
+//}
+
+
+
+//Update list item by id
+SPSecurity.RunWithElevatedPrivileges(delegate ()
+{
+   using (SPSite currentSite = new SPSite(thissite.ID))
+   {
+       using (SPWeb currentWeb = currentSite.OpenWeb(thisweb.ID))
+       {
+
+           SPList formList = currentWeb.Lists["DummyList"];
+           currentWeb.AllowUnsafeUpdates = true;
+
+            SPListItem item = formList.GetItemById(1);
+            item["Title"] = "Test";
+
+            item.Update();
+
+
+        }
+    }
+});
+
+
+
+//Delete List Item
+SPSecurity.RunWithElevatedPrivileges(delegate ()
+{
+   using (SPSite currentSite = new SPSite(thissite.ID))
+   {
+         using (SPWeb currentWeb = currentSite.OpenWeb(thisweb.ID))
+         {
+
+            SPList formList = currentWeb.Lists["DummyList"];
+            currentWeb.AllowUnsafeUpdates = true;
+
+            SPListItem item = formList.GetItemById(1);
+            item.Delete();
+
+
+           }
+    }
+});
