ed72eca3a4a083587ba3391a344578b94a7a7b83
olgaczaim <46277089+olgaczaim@users.noreply.github.com>
2018-12-31 11:45:12 +0300

    Updates

--- Powershell Scripts/Run SP Conf  Wiz.ps1
+++ Powershell Scripts/Run SP Conf  Wiz.ps1
@@ -1,4 +1,4 @@
-//Run sharepoint configuration wizard
+#Run sharepoint configuration wizard
 
 psconfig.exe -cmd helpcollections -installall -cmd securere sources -cmd services -install -cmd installfeatures -cmd  applicationcontent -install -cmd upgrade -inplace b2b -force -wait
  
